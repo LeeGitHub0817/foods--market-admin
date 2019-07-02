cb42191c319eeccd646146c65b61e66320352863
LeeGitHub0817 <18381317534@163.com>
2019-07-02 17:42:33 +0800

    ++

---
+++ src/components/TestSlot.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>子组件-插槽</p>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Two.vue
+++ src/components/Two.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
         <h1>This is page two!</h1>
+        <hr>
+        <t-s>
+            <h1>这个内容是插槽分发过来的</h1>
+            <p>{{ text }}</p>
+        </t-s>
     </div>
 </template>
 
 <script>
+import TestSlot from './TestSlot';
 export default {
-    
+    data: function(){
+        return {
+            text: 'hello slot'
+        }
+    },
+    components: {
+        't-s': TestSlot
+    }
 }
 </script>
 
